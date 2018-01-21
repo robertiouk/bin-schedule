ed0373d9a8f77da84ccdfe4b820b3240271ab75a
robertiouk <robertiouk@gmail.com>
2018-01-21 12:14:45 +0000

    added another test

--- src/test/java/core/BinDaySpeechletTest.java
+++ src/test/java/core/BinDaySpeechletTest.java
@@ -17,6 +17,9 @@ public class BinDaySpeechletTest {
 
         expected = "This weeks collection will be on Tuesday and will be General Rubbish";
         assertEquals(expected, speechlet.getBinScheduleText(ZonedDateTime.of(2018, 1, 15, 20, 42, 30, 0, ZoneId.of("GMT"))));
+
+        expected = "This weeks collection was on Tuesday and was Recycling and Garden";
+        assertEquals(expected, speechlet.getBinScheduleText(ZonedDateTime.of(2018, 10, 17, 9, 0, 0, 0, ZoneId.of("GMT"))));
     }
 
     @Test
